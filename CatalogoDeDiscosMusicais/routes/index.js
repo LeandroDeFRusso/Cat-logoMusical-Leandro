808f61975d97e871bf8229e42deb09240376ae01
LeandroDeFRusso <leandro.russo@edu.unifil.br>
2024-11-24 17:31:40 -0300

    Pequenas atualizações

--- CatalogoDeDiscosMusicais/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { showDiscos } from '../controllers/discosController.js';
-
-const router = express.Router();
-
-router.get('/', showDiscos);
-
-
-export default router;
